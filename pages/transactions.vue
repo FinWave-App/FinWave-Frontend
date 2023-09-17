6be50666ab7191d47966bd1a221cbd3e8464e2bc
isKonstantin <me@knst.su>
2023-09-18 00:47:18 +0300

    Added exchange rate to swap transactions

--- pages/transactions.vue
+++ pages/transactions.vue
@@ -49,7 +49,7 @@
                       </p>
                     </div>
 
-                    <div>
+                    <div class="tooltip btn btn-sm m-0 p-1 btn-ghost z-10" :data-tip="formatExchangeRate(transaction, exchangeRateMode)" @click="exchangeRateMode = !exchangeRateMode">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                       </svg>
@@ -143,6 +143,8 @@ const tagsTree = $transactionsTagsApi.getTagsTree();
 
 const currenciesMap = $currenciesApi.getCurrenciesMap();
 
+const exchangeRateMode = ref(false);
+
 const page = ref(1);
 const maxPages = computed(() => Math.ceil(count.value / countAtPage));
 
@@ -163,6 +165,21 @@ const formatDelta = (delta, currencyId) => {
     maximumFractionDigits: currency.decimals
   });
   return formatter.format(delta).replace(currency.code + " ", currency.symbol).replace(" " + currency.code, " " + currency.symbol);
+}
+
+const formatExchangeRate = (transaction, mode) => {
+  const linked = transaction.metadata.linkedTransaction;
+
+  const transaction1 = mode ? transaction : linked;
+  const transaction2 = mode ? linked : transaction;
+
+  const currency1 = currenciesMap.value.get(transaction1.currencyId);
+  const currency2 = currenciesMap.value.get(transaction2.currencyId);
+
+  const delta1 = Math.abs(transaction1.delta);
+  const delta2 = Math.abs(transaction2.delta);
+
+  return currency2.code + "/" + currency1.code + ": " + formatDelta(delta1 / delta2, (transaction1.currencyId));
 }
 
 const fetchCount = async () => {
